e21d0dbb3b5231a81cc21897f8fa44e7e7b2c546
corlin <me@corlin.cn>
2021-08-20 16:27:43 +0800

    更新
    jupyter重构定版

--- 05jupyternew/deploy.sh
+++ 05jupyternew/deploy.sh
@@ -4,10 +4,11 @@ docker build -t corlin/jupyter .
 docker tag  corlin/jupyter  corlin/jupyter:miniforge4.10.0.0-pyalink1.4.0-xgboost1.1.1
 #docker tag  corlin/jupyter:miniforge4.10.0.0-pyalink1.4.0-xgboost1.1.1 corlin/jupyter
 docker push  corlin/jupyter
-docker push  corlin/jupyter corlin/jupyter:miniforge4.10.0.0-pyalink1.4.0-xgboost1.1.1
+docker push  corlin/jupyter:miniforge4.10.0.0-pyalink1.4.0-xgboost1.1.1
 docker rmi  corlin/jupyter corlin/jupyter:miniforge4.10.0.0-pyalink1.4.0-xgboost1.1.1
 docker rmi  corlin/jupyter
 
+ docker save corlin/jupyter:miniforge4.10.0.0-pyalink1.4.0-xgboost1.1.1 > /data/jupyter0.2.tar
 
 docker run \
         --rm -p 8888:8888 \
