06bd4668874936d4d16a420f8ae485a7873a2a4e
corlin <me@corlin.cn>
2021-08-20 14:37:43 +0800

    更新jupyter

---
+++ 05jupyternew/deploy.sh
@@ -0,0 +1,16 @@
+
+
+docker build -t corlin/jupyter .
+docker tag  corlin/jupyter  corlin/jupyter:miniforge4.10.0.0-pyalink1.4.0-xgboost1.1.1
+#docker tag  corlin/jupyter:miniforge4.10.0.0-pyalink1.4.0-xgboost1.1.1 corlin/jupyter
+docker push  corlin/jupyter
+docker push  corlin/jupyter corlin/jupyter:miniforge4.10.0.0-pyalink1.4.0-xgboost1.1.1
+docker rmi  corlin/jupyter corlin/jupyter:miniforge4.10.0.0-pyalink1.4.0-xgboost1.1.1
+docker rmi  corlin/jupyter
+
+
+docker run \
+        --rm -p 8888:8888 \
+        -e JUPYTER_ENABLE_LAB=yes \
+        -v "${PWD}":/home/jovyan/work \
+        corlin/jupyter:latest
