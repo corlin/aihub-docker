6074d100e7ccdf20803855843ee0c3cd76cf9d03
corlin <me@corlin.cn>
2021-08-31 18:51:28 +0800

    修改
    修改

--- 07jhub/jupyterhub_config.py
+++ 07jhub/jupyterhub_config.py
@@ -7,7 +7,7 @@
 
 ## The date format used by logging formatters for %(asctime)s
 #  Default: '%Y-%m-%d %H:%M:%S'
-# c.Application.log_datefmt = '%Y-%m-%d %H:%M:%S'
+c.Application.log_datefmt = '%Y-%m-%d %H:%M:%S'
 
 ## The Logging format template
 #  Default: '[%(name)s]%(highlevel)s %(message)s'
@@ -16,7 +16,7 @@
 ## Set the log level by value or name.
 #  Choices: any of [0, 10, 20, 30, 40, 50, 'DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL']
 #  Default: 30
-# c.Application.log_level = 30
+c.Application.log_level = 'INFO'
 
 ## Instead of starting the Application, dump configuration to stdout
 #  Default: False
@@ -51,24 +51,17 @@
 #  Default: 1800
 # c.JupyterHub.active_user_window = 1800
 
-## Resolution (in seconds) for updating activity
-#  
-#  If activity is registered that is less than activity_resolution seconds more
-#  recent than the current value, the new value will be ignored.
-#  
-#  This avoids too many writes to the Hub database.
-#  Default: 30
-# c.JupyterHub.activity_resolution = 30
-
 ## Grant admin users permission to access single-user servers.
 #  
 #  Users should be properly informed if this is enabled.
 #  Default: False
-# c.JupyterHub.admin_access = False
+c.JupyterHub.admin_access = True
 
 ## DEPRECATED since version 0.7.2, use Authenticator.admin_users instead.
 #  Default: set()
-# c.JupyterHub.admin_users = set()
+c.JupyterHub.admin_users = {'corlin'}
+c.LocalAuthenticator.create_system_users=True
+c.LocalAuthenticator.add_user_cmd='/home/corlin/addu'e
 
 ## Allow named single-user servers per user
 #  Default: False
@@ -78,14 +71,15 @@
 #  Default: False
 # c.JupyterHub.answer_yes = False
 
-## PENDING DEPRECATION: consider using services
+## PENDING DEPRECATION: consider using service_tokens
 #  
 #  Dict of token:username to be loaded into the database.
 #  
 #  Allows ahead-of-time generation of API tokens for use by externally managed
 #  services, which authenticate as JupyterHub users.
 #  
-#  Consider using services for general services that talk to the JupyterHub API.
+#  Consider using service_tokens for general services that talk to the JupyterHub
+#  API.
 #  Default: {}
 # c.JupyterHub.api_tokens = {}
 
@@ -193,8 +187,8 @@
 #  Loaded from the JPY_COOKIE_SECRET env variable by default.
 #  
 #  Should be exactly 256 bits (32 bytes).
-#  Default: traitlets.Undefined
-# c.JupyterHub.cookie_secret = traitlets.Undefined
+#  Default: b''
+# c.JupyterHub.cookie_secret = b''
 
 ## File in which to store the cookie secret.
 #  Default: 'jupyterhub_cookie_secret'
@@ -221,29 +215,11 @@
 #  Default: False
 # c.JupyterHub.debug_proxy = False
 
-## If named servers are enabled, default name of server to spawn or open, e.g. by
-#  user-redirect.
-#  Default: ''
-# c.JupyterHub.default_server_name = ''
-
 ## The default URL for users when they arrive (e.g. when user directs to "/")
 #  
 #  By default, redirects users to their own server.
-#  
-#  Can be a Unicode string (e.g. '/hub/home') or a callable based on the handler
-#  object:
-#  
-#  ::
-#  
-#      def default_url_fn(handler):
-#          user = handler.current_user
-#          if user and user.admin:
-#              return '/hub/admin'
-#          return '/hub/home'
-#  
-#      c.JupyterHub.default_url = default_url_fn
-#  Default: traitlets.Undefined
-# c.JupyterHub.default_url = traitlets.Undefined
+#  Default: ''
+# c.JupyterHub.default_url = ''
 
 ## Dict authority:dict(files). Specify the key, cert, and/or ca file for an
 #  authority. This is useful for externally managed proxies that wish to use
@@ -305,11 +281,11 @@
 
 ## The ip or hostname for proxies and spawners to use for connecting to the Hub.
 #  
-#  Use when the bind address (`hub_ip`) is 0.0.0.0, :: or otherwise different
-#  from the connect address.
+#  Use when the bind address (`hub_ip`) is 0.0.0.0 or otherwise different from
+#  the connect address.
 #  
-#  Default: when `hub_ip` is 0.0.0.0 or ::, use `socket.gethostname()`, otherwise
-#  use `hub_ip`.
+#  Default: when `hub_ip` is 0.0.0.0, use `socket.gethostname()`, otherwise use
+#  `hub_ip`.
 #  
 #  Note: Some spawners or proxy implementations might not support hostnames.
 #  Check your spawner or proxy documentation to see if they have extra
@@ -366,56 +342,6 @@
 #  See also `hub_ip` for the ip and `hub_bind_url` for setting the full bind URL.
 #  Default: 8081
 # c.JupyterHub.hub_port = 8081
-
-## The routing prefix for the Hub itself.
-#  
-#  Override to send only a subset of traffic to the Hub. Default is to use the
-#  Hub as the default route for all requests.
-#  
-#  This is necessary for normal jupyterhub operation, as the Hub must receive
-#  requests for e.g. `/user/:name` when the user's server is not running.
-#  
-#  However, some deployments using only the JupyterHub API may want to handle
-#  these events themselves, in which case they can register their own default
-#  target with the proxy and set e.g. `hub_routespec = /hub/` to serve only the
-#  hub's own pages, or even `/hub/api/` for api-only operation.
-#  
-#  Note: hub_routespec must include the base_url, if any.
-#  
-#  .. versionadded:: 1.4
-#  Default: '/'
-# c.JupyterHub.hub_routespec = '/'
-
-## Trigger implicit spawns after this many seconds.
-#  
-#  When a user visits a URL for a server that's not running, they are shown a
-#  page indicating that the requested server is not running with a button to
-#  spawn the server.
-#  
-#  Setting this to a positive value will redirect the user after this many
-#  seconds, effectively clicking this button automatically for the users,
-#  automatically beginning the spawn process.
-#  
-#  Warning: this can result in errors and surprising behavior when sharing access
-#  URLs to actual servers, since the wrong server is likely to be started.
-#  Default: 0
-# c.JupyterHub.implicit_spawn_seconds = 0
-
-## Timeout (in seconds) to wait for spawners to initialize
-#  
-#  Checking if spawners are healthy can take a long time if many spawners are
-#  active at hub start time.
-#  
-#  If it takes longer than this timeout to check, init_spawner will be left to
-#  complete in the background and the http server is allowed to start.
-#  
-#  A timeout of -1 means wait forever, which can mean a slow startup of the Hub
-#  but ensures that the Hub is fully consistent by the time it starts responding
-#  to requests. This matches the behavior of jupyterhub 1.0.
-#  
-#  .. versionadded: 1.1.0
-#  Default: 10
-# c.JupyterHub.init_spawners_timeout = 10
 
 ## The location to store certificates automatically created by JupyterHub.
 #  
@@ -486,30 +412,6 @@
 #  Default: 0
 # c.JupyterHub.named_server_limit_per_user = 0
 
-## Expiry (in seconds) of OAuth access tokens.
-#  
-#  The default is to expire when the cookie storing them expires, according to
-#  `cookie_max_age_days` config.
-#  
-#  These are the tokens stored in cookies when you visit a single-user server or
-#  service. When they expire, you must re-authenticate with the Hub, even if your
-#  Hub authentication is still valid. If your Hub authentication is valid,
-#  logging in may be a transparent redirect as you refresh the page.
-#  
-#  This does not affect JupyterHub API tokens in general, which do not expire by
-#  default. Only tokens issued during the oauth flow accessing services and
-#  single-user servers are affected.
-#  
-#  .. versionadded:: 1.4
-#      OAuth token expires_in was not previously configurable.
-#  .. versionchanged:: 1.4
-#      Default now uses cookie_max_age_days so that oauth tokens
-#      which are generally stored in cookies,
-#      expire when the cookies storing them expire.
-#      Previously, it was one hour.
-#  Default: 0
-# c.JupyterHub.oauth_token_expires_in = 0
-
 ## File to write PID Useful for daemonizing JupyterHub.
 #  Default: ''
 # c.JupyterHub.pid_file = ''
@@ -536,9 +438,9 @@
 #  Default: ''
 # c.JupyterHub.proxy_auth_token = ''
 
-## DEPRECATED since version 0.8: Use ConfigurableHTTPProxy.check_running_interval
-#  Default: 5
-# c.JupyterHub.proxy_check_interval = 5
+## Interval (in seconds) at which to check if the proxy is running.
+#  Default: 30
+# c.JupyterHub.proxy_check_interval = 30
 
 ## The class to use for configuring the JupyterHub proxy.
 #  
@@ -735,18 +637,6 @@
 #  Default: False
 # c.JupyterHub.upgrade_db = False
 
-## Callable to affect behavior of /user-redirect/
-#  
-#  Receives 4 parameters: 1. path - URL path that was provided after /user-
-#  redirect/ 2. request - A Tornado HTTPServerRequest representing the current
-#  request. 3. user - The currently authenticated user. 4. base_url - The
-#  base_url of the current hub, for relative redirects
-#  
-#  It should return the new URL to redirect to, or None to preserve current
-#  behavior.
-#  Default: None
-# c.JupyterHub.user_redirect_hook = None
-
 #------------------------------------------------------------------------------
 # Spawner(LoggingConfigurable) configuration
 #------------------------------------------------------------------------------
@@ -767,20 +657,6 @@
 #  documentation for your spawner to verify!
 #  Default: []
 # c.Spawner.args = []
-
-## An optional hook function that you can implement to pass `auth_state` to the
-#  spawner after it has been initialized but before it starts. The `auth_state`
-#  dictionary may be set by the `.authenticate()` method of the authenticator.
-#  This hook enables you to pass some or all of that information to your spawner.
-#  
-#  Example::
-#  
-#      def userdata_hook(spawner, auth_state):
-#          spawner.userdata = auth_state["userdata"]
-#  
-#      c.Spawner.auth_state_hook = userdata_hook
-#  Default: None
-# c.Spawner.auth_state_hook = None
 
 ## The command used for starting the single-user server.
 #  
@@ -864,20 +740,20 @@
 #  Default: False
 # c.Spawner.disable_user_config = False
 
-## List of environment variables for the single-user server to inherit from the
-#  JupyterHub process.
+## Whitelist of environment variables for the single-user server to inherit from
+#  the JupyterHub process.
 #  
-#  This list is used to ensure that sensitive information in the JupyterHub
+#  This whitelist is used to ensure that sensitive information in the JupyterHub
 #  process's environment (such as `CONFIGPROXY_AUTH_TOKEN`) is not passed to the
 #  single-user server's process.
-#  Default: ['PATH', 'PYTHONPATH', 'CONDA_ROOT', 'CONDA_DEFAULT_ENV', 'VIRTUAL_ENV', 'LANG', 'LC_ALL', 'JUPYTERHUB_SINGLEUSER_APP']
-# c.Spawner.env_keep = ['PATH', 'PYTHONPATH', 'CONDA_ROOT', 'CONDA_DEFAULT_ENV', 'VIRTUAL_ENV', 'LANG', 'LC_ALL', 'JUPYTERHUB_SINGLEUSER_APP']
+#  Default: ['PATH', 'PYTHONPATH', 'CONDA_ROOT', 'CONDA_DEFAULT_ENV', 'VIRTUAL_ENV', 'LANG', 'LC_ALL']
+# c.Spawner.env_keep = ['PATH', 'PYTHONPATH', 'CONDA_ROOT', 'CONDA_DEFAULT_ENV', 'VIRTUAL_ENV', 'LANG', 'LC_ALL']
 
 ## Extra environment variables to set for the single-user server's process.
 #  
 #  Environment variables that end up in the single-user server's process come from 3 sources:
 #    - This `environment` configurable
-#    - The JupyterHub process' environment variables that are listed in `env_keep`
+#    - The JupyterHub process' environment variables that are whitelisted in `env_keep`
 #    - Variables to establish contact between the single-user notebook and the hub (such as JUPYTERHUB_API_TOKEN)
 #  
 #  The `environment` configurable should be set by JupyterHub administrators to
@@ -890,11 +766,6 @@
 #  Note that the spawner class' interface is not guaranteed to be exactly same
 #  across upgrades, so if you are using the callable take care to verify it
 #  continues to work after upgrades!
-#  
-#  .. versionchanged:: 1.2
-#      environment from this configuration has highest priority,
-#      allowing override of 'default' env variables,
-#      such as JUPYTERHUB_API_URL.
 #  Default: {}
 # c.Spawner.environment = {}
 
@@ -904,15 +775,6 @@
 #  wait before assuming that the server is unable to accept connections.
 #  Default: 30
 # c.Spawner.http_timeout = 30
-
-## The URL the single-user server should connect to the Hub.
-#  
-#  If the Hub URL set in your JupyterHub config is not reachable from spawned
-#  notebooks, you can set differnt URL by this config.
-#  
-#  Is None if you don't need to change the URL.
-#  Default: None
-# c.Spawner.hub_connect_url = None
 
 ## The IP address (or hostname) the single-user server should listen on.
 #  
@@ -997,32 +859,6 @@
 #  so using this functionality might cause your JupyterHub upgrades to break.
 #  Default: traitlets.Undefined
 # c.Spawner.options_form = traitlets.Undefined
-
-## Interpret HTTP form data
-#  
-#  Form data will always arrive as a dict of lists of strings. Override this
-#  function to understand single-values, numbers, etc.
-#  
-#  This should coerce form data into the structure expected by self.user_options,
-#  which must be a dict, and should be JSON-serializeable, though it can contain
-#  bytes in addition to standard JSON data types.
-#  
-#  This method should not have any side effects. Any handling of `user_options`
-#  should be done in `.start()` to ensure consistent behavior across servers
-#  spawned via the API and form submission page.
-#  
-#  Instances will receive this data on self.user_options, after passing through
-#  this function, prior to `Spawner.start`.
-#  
-#  .. versionchanged:: 1.0
-#      user_options are persisted in the JupyterHub database to be reused
-#      on subsequent spawns if no options are given.
-#      user_options is serialized to JSON as part of this persistence
-#      (with additional support for bytes in case of uploaded file data),
-#      and any non-bytes non-jsonable values will be replaced with None
-#      if the user_options are re-used.
-#  Default: traitlets.Undefined
-# c.Spawner.options_from_form = traitlets.Undefined
 
 ## Interval (in seconds) on which to poll the spawner for single-user server's
 #  status.
@@ -1111,19 +947,6 @@
 #  Default: set()
 # c.Authenticator.admin_users = set()
 
-## Set of usernames that are allowed to log in.
-#  
-#  Use this with supported authenticators to restrict which users can log in.
-#  This is an additional list that further restricts users, beyond whatever
-#  restrictions the authenticator has in place.
-#  
-#  If empty, does not perform any additional restriction.
-#  
-#  .. versionchanged:: 1.2
-#      `Authenticator.whitelist` renamed to `allowed_users`
-#  Default: set()
-# c.Authenticator.allowed_users = set()
-
 ## The max age (in seconds) of authentication info before forcing a refresh of
 #  user auth info.
 #  
@@ -1146,34 +969,17 @@
 #  Default: False
 # c.Authenticator.auto_login = False
 
-## Set of usernames that are not allowed to log in.
+## Blacklist of usernames that are not allowed to log in.
 #  
 #  Use this with supported authenticators to restrict which users can not log in.
-#  This is an additional block list that further restricts users, beyond whatever
+#  This is an additional blacklist that further restricts users, beyond whatever
 #  restrictions the authenticator has in place.
 #  
 #  If empty, does not perform any additional restriction.
 #  
 #  .. versionadded: 0.9
-#  
-#  .. versionchanged:: 1.2
-#      `Authenticator.blacklist` renamed to `blocked_users`
 #  Default: set()
-# c.Authenticator.blocked_users = set()
-
-## Delete any users from the database that do not pass validation
-#  
-#  When JupyterHub starts, `.add_user` will be called on each user in the
-#  database to verify that all users are still valid.
-#  
-#  If `delete_invalid_users` is True, any users that do not pass validation will
-#  be deleted from the database. Use this if users might be deleted from an
-#  external system, such as local user accounts.
-#  
-#  If False (default), invalid users remain in the Hub's database and a warning
-#  will be issued. This is the default to avoid data loss due to config changes.
-#  Default: False
-# c.Authenticator.delete_invalid_users = False
+# c.Authenticator.blacklist = set()
 
 ## Enable persisting auth_state (if available).
 #  
@@ -1253,7 +1059,13 @@
 #  Default: ''
 # c.Authenticator.username_pattern = ''
 
-## Deprecated, use `Authenticator.allowed_users`
+## Whitelist of usernames that are allowed to log in.
+#  
+#  Use this with supported authenticators to restrict which users can log in.
+#  This is an additional whitelist that further restricts users, beyond whatever
+#  restrictions the authenticator has in place.
+#  
+#  If empty, does not perform any additional restriction.
 #  Default: set()
 # c.Authenticator.whitelist = set()
 
@@ -1270,14 +1082,3 @@
 ## The number of threads to allocate for encryption
 #  Default: 8
 # c.CryptKeeper.n_threads = 8
-
-#------------------------------------------------------------------------------
-# Pagination(Configurable) configuration
-#------------------------------------------------------------------------------
-## Default number of entries per page for paginated results.
-#  Default: 100
-# c.Pagination.default_per_page = 100
-
-## Maximum number of entries per page for paginated results.
-#  Default: 250
-# c.Pagination.max_per_page = 250
